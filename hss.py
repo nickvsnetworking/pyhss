00b90880d56601b9fdec57f996c202d12f0b275b
Nick <nick@nickvsnetworking.com>
2019-06-21 17:57:28 +1000

    Added basic untested Gx credit control

--- hss.py
+++ hss.py
@@ -38,7 +38,6 @@ while True:
             packet_length = diameter.decode_diameter_packet_length(data)    #Calculate length of packet from start of packet
             data_sum = data + connection.recv(packet_length - 32)           #Recieve remainder of packet from buffer
             packet_vars, avps = diameter.decode_diameter_packet(data_sum)   #Decode packet into array of AVPs and Dict of Packet Variables (packet_vars)
-            print("Flags are " + str(packet_vars['flags']))
 
             #Send Capabilities Exchange Answer (CEA) response to Capabilites Exchange Request (CER)
             if packet_vars['command_code'] == 257 and packet_vars['ApplicationId'] == 0 and packet_vars['flags'] == "80":
@@ -52,6 +51,10 @@ while True:
                 response = diameter.Answer_280(packet_vars, avps)   #Generate Diameter packet
                 connection.sendall(bytes.fromhex(response))         #Send it
 
+##                time.sleep(1)
+##                request = diameter.Request_16777251_318()
+##                connection.sendall(bytes.fromhex(request))
+
 
             #Send Disconnect Peer Answer (DPA) to Disconnect Peer Request (DPR)
             elif packet_vars['command_code'] == 282 and packet_vars['ApplicationId'] == 0 and packet_vars['flags'] == "80":
@@ -62,6 +65,10 @@ while True:
 
             else:
                 print("Recieved packet with Command Code: " + str(packet_vars['command_code']) + ", ApplicationID: " + str(packet_vars['ApplicationId']) + " and flags " + str(packet_vars['flags']))
+                for keys in packet_vars:
+                    print(keys)
+                    print("\t" + str(packet_vars[keys]))
+                print(avps)
                 print("Panicking and exiting")
                 connection.close()
                 sys.exit()
