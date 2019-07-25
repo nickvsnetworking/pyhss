84820ca66e17488891d6dcddbf55da0069e94468
nickvsnetworking <nick@nickvsnetworking.com>
2019-07-24 19:51:42 -0700

    Added Gx and S6a support in CEA

--- diameter.py
+++ diameter.py
@@ -232,7 +232,8 @@ def Answer_257(packet_vars, avps):
     avp += generate_avp(266, 40, "00000000")                                                    #Vendor-Id
     avp += generate_avp(269, 40, string_to_hex("PyHSS"))                                        #Product-Name
     avp += generate_avp(267, 40, "000027d9")                                                    #Firmware-Revision
-    avp += generate_avp(260, 40, "000001024000000c010000160000010a4000000c000028af")            #Vendor-Specific-Application-ID
+    avp += generate_avp(260, 40, "000001024000000c010000160000010a4000000c000028af")            #Vendor-Specific-Application-ID (Gx)
+    avp += generate_avp(260, 40, "000001024000000c01000023" + "000001024000000c01000016" + "0000010a4000000c000028af")            #Vendor-Specific-Application-ID (S6a)
     avp += generate_avp(258, 40, "ffffffff")                                                    #Auth-Application-ID
     avp += generate_avp(265, 40, "0000159f")                                                    #Supported-Vendor-ID (3GGP v2)
     avp += generate_avp(265, 40, "000028af")                                                    #Supported-Vendor-ID (3GPP)
@@ -250,7 +251,7 @@ def Answer_257(packet_vars, avps):
 def Answer_280(packet_vars, avps):                                                      
     avp = ''                                                                                    #Initiate empty var AVP 
     avp += generate_avp(268, 40, "000007d1")                                                    #Result Code (DIAMETER_SUCESS (2001))
-    avp += generate_avp(264, 40, str(binascii.hexlify(b'nickpc.localdomain'),'ascii'))            #Origin Host
+    avp += generate_avp(264, 40, str(binascii.hexlify(b'nickpc.localdomain'),'ascii'))          #Origin Host
     avp += generate_avp(296, 40, str(binascii.hexlify(b'localdomain'),'ascii'))                 #Origin Realm
     for avps_to_check in avps:                                                                  #Only include AVP 278 (Origin State) if inital request included it
         if avps_to_check['avp_code'] == 278:                                
