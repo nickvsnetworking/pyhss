538883d9e4fa54ec09f2d380e968b590d36ce46d
nickvsnetworking <nick@nickvsnetworking.com>
2021-03-08 15:25:14 +1100

    Single transport support (TCP or SCTP) in same server

--- hss.py
+++ hss.py
@@ -16,8 +16,10 @@ import socketserver
 import diameter
 import binascii
 import time
+import _thread
 from threading import Thread, Lock
 import os
+import sctp
 import traceback
 
 logging.info("Current config file values:")
@@ -26,32 +28,28 @@ for config_sections in yaml_config:
     for lower_keys in yaml_config[config_sections]:
         logging.info("\t\t" + str(lower_keys) + "\t" + str(yaml_config[config_sections][lower_keys]))
 
+
+
 #Initialize Diameter
 diameter = diameter.Diameter(str(yaml_config['hss']['OriginHost']), str(yaml_config['hss']['OriginRealm']), str(yaml_config['hss']['ProductName']), str(yaml_config['hss']['MNC']), str(yaml_config['hss']['MCC']))
 
-class DiameterRequestHandler(socketserver.BaseRequestHandler):
-    print('PyHSS started - listening on ' + str(yaml_config['hss']['bind_ip'][0]) + ' port ' + str(yaml_config['hss']['bind_port']))
+def on_new_client(clientsocket,client_address):
+    print('New connection from ' + str(client_address))
     data_sum = b''
-    firstloop = 0
-    def setup(self):
-        logging.info('New connection from ' + str(self.client_address[0]))
-        while 1:
-            data = self.request.recv(32)
+    while True:
+        try:
+            data = clientsocket.recv(32)
             if not data:
-                logging.info("Connection closed by " + str(self.client_address[0]))
+                print("Connection closed by " + str(client_address))
                 break
-            try:            
-                packet_length = diameter.decode_diameter_packet_length(data)            #Calculate length of packet from start of packet
-                data_sum = data + self.request.recv(packet_length - 32)                 #Recieve remainder of packet from buffer
-                packet_vars, avps = diameter.decode_diameter_packet(data_sum)           #Decode packet into array of AVPs and Dict of Packet Variables (packet_vars)
+            
+            packet_length = diameter.decode_diameter_packet_length(data)            #Calculate length of packet from start of packet
+            data_sum = data + clientsocket.recv(packet_length - 32)           #Recieve remainder of packet from buffer
+            packet_vars, avps = diameter.decode_diameter_packet(data_sum)   #Decode packet into array of AVPs and Dict of Packet Variables (packet_vars)
 
-                orignHost = diameter.get_avp_data(avps, 264)[0]                         #Get OriginHost from AVP
-                orignHost = binascii.unhexlify(orignHost).decode('utf-8')               #Format it
-            except Exception as e:
-                logging.info("Failed to get packet_vars and AVPs from " + str(self.client_address[0]))
-                logging.info(e)
-                traceback.print_exc()
-                
+            orignHost = diameter.get_avp_data(avps, 264)[0]                         #Get OriginHost from AVP
+            orignHost = binascii.unhexlify(orignHost).decode('utf-8')               #Format it
+
 
             #Send Capabilities Exchange Answer (CEA) response to Capabilites Exchange Request (CER)
             if packet_vars['command_code'] == 257 and packet_vars['ApplicationId'] == 0 and packet_vars['flags'] == "80":                    
@@ -131,29 +129,67 @@ class DiameterRequestHandler(socketserver.BaseRequestHandler):
                 response = diameter.Answer_16777291_8388622(packet_vars, avps)      #Generate Diameter packet
 
 
-            
 
             else:
-                logging.info("\n\nRecieved unrecognised request with Command Code: " + str(packet_vars['command_code']) + ", ApplicationID: " + str(packet_vars['ApplicationId']) + " and flags " + str(packet_vars['flags']))
+                print("\n\nRecieved unrecognised request with Command Code: " + str(packet_vars['command_code']) + ", ApplicationID: " + str(packet_vars['ApplicationId']) + " and flags " + str(packet_vars['flags']))
                 for keys in packet_vars:
-                    logging.debug(keys)
-                    logging.debug("\t" + str(packet_vars[keys]))
-                logging.debug(avps)
-                logging.info("Sending negative response")
-                packet_vars, avps
-                response = diameter.Respond_ResultCode(packet_vars, avps, 3001)      #Generate 	DIAMETER_COMMAND_UNSUPPORTED response
-                
+                    print(keys)
+                    print("\t" + str(packet_vars[keys]))
+                print(avps)
+                print("Sending negative response")
+                response = diameter.Respond_Command_Unsupported(packet_vars, avps)      #Generate Diameter packet
+                clientsocket.sendall(bytes.fromhex(response))                           #Send it
 
-
+            #Handle actual sending
             try:
-                self.request.sendall(bytes.fromhex(response))                           #Send it
+                clientsocket.sendall(bytes.fromhex(response))                           #Send it
             except Exception as e:
                     logging.info("Failed to send Diameter Response")
                     logging.debug("Diameter Response Body: " + str(response))
                     logging.info(e)
                     traceback.print_exc()
 
+                    
+        except KeyboardInterrupt:
+            #Clean up the connection on keyboard interupt
+            response = diameter.Request_282()                       #Generate Disconnect Peer Request Diameter packet
+            clientsocket.sendall(bytes.fromhex(response))             #Send it
+            clientsocket.close()
+            print("Connection closed")
+            sys.exit()
+    
 
-server = socketserver.ThreadingTCPServer((str(yaml_config['hss']['bind_ip'][0]), int(yaml_config['hss']['bind_port'])), DiameterRequestHandler)
+if yaml_config['hss']['transport'] == "SCTP":
+    logging.debug("Using SCTP for Transport")
+    # Create a SCTP socket
+    sock = sctp.sctpsocket_tcp(socket.AF_INET)
+    # Loop through the possible Binding IPs from the config and bind to each for Multihoming
+    server_addresses = []
+    for host in yaml_config['hss']['bind_ip']:
+        logging.info("Seting up SCTP binding on local IP address " + str(host))
+        server_addresses.append((str(host), int(yaml_config['hss']['bind_port'])))
+    sock.bindx(server_addresses)
+    logging.info("PyHSS listening on SCTP port " + str(server_addresses))
+    # Listen for up to 5 incoming connection
+    sock.listen(5)
+elif yaml_config['hss']['transport'] == "TCP":
+    logging.debug("Using TCP socket")
+    # Create a TCP/IP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # Bind the socket to the port
+    server_address = (str(yaml_config['hss']['bind_ip'][0]), int(yaml_config['hss']['bind_port']))    
+    sock.bind(server_address)
+    logging.debug('PyHSS listening on TCP port ' + str(yaml_config['hss']['bind_ip'][0]))
+    # Listen for up to 1 incoming connection
+    sock.listen(5)
+else:
+    logging.error("No valid transports found (No SCTP or TCP) - Exiting")
+    sys.exit()
 
-server.serve_forever()
+while True:
+    # Wait for a connection
+    print('Waiting for a connection...')
+    connection, client_address = sock.accept()
+    _thread.start_new_thread(on_new_client,(connection,client_address))
+    
+
