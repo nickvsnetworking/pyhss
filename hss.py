4d1a9a57959d898325f06ba151cc4e773e5a03ac
nickvsnetworking <nick@nickvsnetworking.com>
2021-03-10 18:38:46 +1100

    Added try/except handling in HSS service

--- hss.py
+++ hss.py
@@ -54,18 +54,28 @@ def on_new_client(clientsocket,client_address):
             #Send Capabilities Exchange Answer (CEA) response to Capabilites Exchange Request (CER)
             if packet_vars['command_code'] == 257 and packet_vars['ApplicationId'] == 0 and packet_vars['flags'] == "80":                    
                 logging.info("Received Request with command code 257 (CER) from " + orignHost + "\n\tSending response (CEA)")
-                response = diameter.Answer_257(packet_vars, avps, str(yaml_config['hss']['bind_ip'][0]))                   #Generate Diameter packet
+                try:
+                    response = diameter.Answer_257(packet_vars, avps, str(yaml_config['hss']['bind_ip'][0]))                   #Generate Diameter packet
+                except:
+                    response = diameter.Respond_ResultCode(packet_vars, avps, 5012)      #Generate Diameter response with "DIAMETER_UNABLE_TO_COMPLY" (5012)
 
 
             #Send Credit Control Answer (CCA) response to Credit Control Request (CCR)
             elif packet_vars['command_code'] == 272 and packet_vars['ApplicationId'] == 16777238:
                 logging.info("Received 3GPP Credit-Control-Request from " + orignHost + "\n\tGenerating (CCA)")
-                response = diameter.Answer_16777238_272(packet_vars, avps)          #Generate Diameter packet
+                try:
+                    response = diameter.Answer_16777238_272(packet_vars, avps)          #Generate Diameter packet
+                except:
+                    response = diameter.Respond_ResultCode(packet_vars, avps, 5012)      #Generate Diameter response with "DIAMETER_UNABLE_TO_COMPLY" (5012)
+
 
             #Send Device Watchdog Answer (DWA) response to Device Watchdog Requests (DWR)
             elif packet_vars['command_code'] == 280 and packet_vars['ApplicationId'] == 0 and packet_vars['flags'] == "80":
                 logging.info("Received Request with command code 280 (DWR) from " + orignHost + "\n\tSending response (DWA)")
-                response = diameter.Answer_280(packet_vars, avps)                   #Generate Diameter packet
+                try:
+                    response = diameter.Answer_280(packet_vars, avps)                   #Generate Diameter packet
+                except:
+                    response = diameter.Respond_ResultCode(packet_vars, avps, 5012)      #Generate Diameter response with "DIAMETER_UNABLE_TO_COMPLY" (5012)
 
             #Send Disconnect Peer Answer (DPA) response to Disconnect Peer Request (DPR)
             elif packet_vars['command_code'] == 282 and packet_vars['ApplicationId'] == 0 and packet_vars['flags'] == "80":
@@ -86,49 +96,102 @@ def on_new_client(clientsocket,client_address):
             #S6a Update Location Answer (ULA) response to Update Location Request (ULR)
             elif packet_vars['command_code'] == 316 and packet_vars['ApplicationId'] == 16777251:
                 logging.info("Received Request with command code 316 (3GPP Update Location-Request) from " + orignHost + "\n\tGenerating (ULA)")
-                response = diameter.Answer_16777251_316(packet_vars, avps)      #Generate Diameter packet
+                try:
+                    response = diameter.Answer_16777251_316(packet_vars, avps)      #Generate Diameter packet
+                except Exception as e:
+                    logging.info("Failed to generate Diameter Response for ULR")
+                    logging.info(e)
+                    traceback.print_exc()
+                    response = diameter.Respond_ResultCode(packet_vars, avps, 4100) #DIAMETER_USER_DATA_NOT_AVAILABLE
 
             #S6a Purge UE Answer (PUA) response to Purge UE Request (PUR)
             elif packet_vars['command_code'] == 321 and packet_vars['ApplicationId'] == 16777251:
                 logging.info("Received Request with command code 321 (3GPP Purge UE Request) from " + orignHost + "\n\tGenerating (PUA)")
-                response = diameter.Answer_16777251_321(packet_vars, avps)      #Generate Diameter packet
+                try:
+                    response = diameter.Answer_16777251_321(packet_vars, avps)      #Generate Diameter packet
+                except:
+                    response = diameter.Respond_ResultCode(packet_vars, avps, 5012)      #Generate Diameter response with "DIAMETER_UNABLE_TO_COMPLY" (5012)
+
 
             #S6a Purge UE Answer (NOA) response to Notify Request (NOR)
             elif packet_vars['command_code'] == 323 and packet_vars['ApplicationId'] == 16777251:
                 logging.info("Received Request with command code 323 (3GPP Notify Request) from " + orignHost + "\n\tGenerating (NOA)")
-                response = diameter.Answer_16777251_323(packet_vars, avps)      #Generate Diameter packet
+                try:
+                    response = diameter.Answer_16777251_323(packet_vars, avps)      #Generate Diameter packet
+                except:
+                    response = diameter.Respond_ResultCode(packet_vars, avps, 5012)      #Generate Diameter response with "DIAMETER_UNABLE_TO_COMPLY" (5012)
+
 
             #Cx Authentication Answer
             elif packet_vars['command_code'] == 300 and packet_vars['ApplicationId'] == 16777216:
                 logging.info("Received Request with command code 300 (3GPP Cx User Authentication Request) from " + orignHost + "\n\tGenerating (MAA)")
-                response = diameter.Answer_16777216_300(packet_vars, avps)      #Generate Diameter packet
+                try:
+                    response = diameter.Answer_16777216_300(packet_vars, avps)      #Generate Diameter packet
+                except Exception as e:
+                    logging.info("Failed to generate Diameter Response for ULR")
+                    logging.info(e)
+                    traceback.print_exc()
+                    response = diameter.Respond_ResultCode(packet_vars, avps, 4100) #DIAMETER_USER_DATA_NOT_AVAILABLE
+
 
             #Cx Server Assignment Answer
             elif packet_vars['command_code'] == 301 and packet_vars['ApplicationId'] == 16777216:
                 logging.info("Received Request with command code 301 (3GPP Cx Server Assignemnt Request) from " + orignHost + "\n\tGenerating (MAA)")
-                response = diameter.Answer_16777216_301(packet_vars, avps)      #Generate Diameter packet
+                try:
+                    response = diameter.Answer_16777216_301(packet_vars, avps)      #Generate Diameter packet
+                except Exception as e:
+                    logging.info("Failed to generate Diameter Response for ULR")
+                    logging.info(e)
+                    traceback.print_exc()
+                    response = diameter.Respond_ResultCode(packet_vars, avps, 4100) #DIAMETER_USER_DATA_NOT_AVAILABLE
+
 
             #Cx Location Information Answer
             elif packet_vars['command_code'] == 302 and packet_vars['ApplicationId'] == 16777216:
                 logging.info("Received Request with command code 302 (3GPP Cx Location Information Request) from " + orignHost + "\n\tGenerating (MAA)")
-                response = diameter.Answer_16777216_302(packet_vars, avps)      #Generate Diameter packet
+                try:
+                    response = diameter.Answer_16777216_302(packet_vars, avps)      #Generate Diameter packet
+                except Exception as e:
+                    logging.info("Failed to generate Diameter Response for ULR")
+                    logging.info(e)
+                    traceback.print_exc()
+                    response = diameter.Respond_ResultCode(packet_vars, avps, 4100) #DIAMETER_USER_DATA_NOT_AVAILABLE
+
 
             #Cx Multimedia Authentication Answer
             elif packet_vars['command_code'] == 303 and packet_vars['ApplicationId'] == 16777216:
                 logging.info("Received Request with command code 303 (3GPP Cx Multimedia Authentication Request) from " + orignHost + "\n\tGenerating (MAA)")
-                response = diameter.Answer_16777216_303(packet_vars, avps)      #Generate Diameter packet
+                try:
+                    response = diameter.Answer_16777216_303(packet_vars, avps)      #Generate Diameter packet
+                except Exception as e:
+                    logging.info("Failed to generate Diameter Response for ULR")
+                    logging.info(e)
+                    traceback.print_exc()
+                    response = diameter.Respond_ResultCode(packet_vars, avps, 4100) #DIAMETER_USER_DATA_NOT_AVAILABLE
+
 
             #S13 ME-Identity-Check Answer
             elif packet_vars['command_code'] == 324 and packet_vars['ApplicationId'] == 16777252:
                 logging.info("Received Request with command code 324 (3GPP S13 ME-Identity-Check Request) from " + orignHost + "\n\tGenerating (MICA)")
-                response = diameter.Answer_16777252_324(packet_vars, avps)      #Generate Diameter packet
+                try:
+                    response = diameter.Answer_16777252_324(packet_vars, avps)      #Generate Diameter packet
+                except Exception as e:
+                    logging.info("Failed to generate Diameter Response for ULR")
+                    logging.info(e)
+                    traceback.print_exc()
+                    response = diameter.Respond_ResultCode(packet_vars, avps, 4100) #DIAMETER_USER_DATA_NOT_AVAILABLE
+
 
             #SLh LCS-Routing-Info-Answer
             elif packet_vars['command_code'] == 8388622 and packet_vars['ApplicationId'] == 16777291:
                 logging.info("Received Request with command code 324 (3GPP SLh LCS-Routing-Info-Answer Request) from " + orignHost + "\n\tGenerating (MICA)")
-                response = diameter.Answer_16777291_8388622(packet_vars, avps)      #Generate Diameter packet
-
-
+                try:
+                    response = diameter.Answer_16777291_8388622(packet_vars, avps)      #Generate Diameter packet
+                except Exception as e:
+                    logging.info("Failed to generate Diameter Response for ULR")
+                    logging.info(e)
+                    traceback.print_exc()
+                    response = diameter.Respond_ResultCode(packet_vars, avps, 4100) #DIAMETER_USER_DATA_NOT_AVAILABLE
 
             else:
                 print("\n\nRecieved unrecognised request with Command Code: " + str(packet_vars['command_code']) + ", ApplicationID: " + str(packet_vars['ApplicationId']) + " and flags " + str(packet_vars['flags']))
@@ -137,7 +200,7 @@ def on_new_client(clientsocket,client_address):
                     print("\t" + str(packet_vars[keys]))
                 print(avps)
                 print("Sending negative response")
-                response = diameter.Respond_Command_Unsupported(packet_vars, avps)      #Generate Diameter packet
+                response = diameter.Respond_ResultCode(packet_vars, avps, 3001)      #Generate Diameter response with "Command Unsupported" (3001)
                 clientsocket.sendall(bytes.fromhex(response))                           #Send it
 
             #Handle actual sending
