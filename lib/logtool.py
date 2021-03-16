76946af59189cb7d3f73447796ca47d7a68b0b06
nickvsnetworking <nick@nickvsnetworking.com>
2021-03-17 07:45:01 +1100

    Added general function for Redis logging to logtool

--- lib/logtool.py
+++ lib/logtool.py
@@ -1,4 +1,24 @@
 import logging
+import os
+import sys
+sys.path.append(os.path.realpath('../'))
+import yaml
+with open("config.yaml", 'r') as stream:
+    yaml_config = (yaml.safe_load(stream))
+
+if yaml_config['redis']['enabled'] == True:
+    logging.debug("Redis support enabled")
+    import redis
+
+#function for handling incrimenting Redis counters with error handling
+def RedisIncrimenter(self, name):
+    if yaml_config['redis']['enabled'] == True:
+        try:
+            self.redis_store.incr(name)
+        except:
+            DiameterLogger.error("failed to incriment " + str(name))
+
+
 def setup_logger(logger_name, log_file, level=logging.DEBUG):
     l = logging.getLogger(logger_name)
     formatter = logging.Formatter('%(asctime)s : %(message)s')
