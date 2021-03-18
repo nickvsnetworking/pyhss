90df3eeca79cceb7ec8793f30d74fe0a4e338d02
nickvsnetworking <nick@nickvsnetworking.com>
2021-03-18 19:17:05 +1100

    Added logging level back into logs

--- lib/logtool.py
+++ lib/logtool.py
@@ -33,7 +33,7 @@ def RedisIncrimenter(name):
 
 def setup_logger(logger_name, log_file, level=logging.DEBUG):
     l = logging.getLogger(logger_name)
-    formatter = logging.Formatter('%(asctime)s : %(message)s')
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     fileHandler = logging.FileHandler(log_file, mode='a+')
     fileHandler.setFormatter(formatter)
     streamHandler = logging.StreamHandler()
