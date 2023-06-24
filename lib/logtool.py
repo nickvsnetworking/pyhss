115e8e694adae569bfd271409f8da4c53bc94def
nickvsnetworking <nick@nickvsnetworking.com>
2023-06-25 09:38:28 +1000

    Removed redis incrimenter and added two new Prometheus Counters

--- lib/logtool.py
+++ lib/logtool.py
@@ -42,6 +42,8 @@ prom_diam_response_time_diam = Histogram('prom_diam_response_time_diam', 'Diamet
 prom_diam_response_time_method = Histogram('prom_diam_response_time_method', 'Diameter Response Times', tags)
 prom_diam_response_time_db = Summary('prom_diam_response_time_db', 'Diameter Response Times from Database')
 prom_diam_response_time_h = Histogram('request_latency_seconds', 'Diameter Response Time Histogram')
+prom_diam_auth_event_count = Counter('prom_diam_auth_event_count', 'Diameter Authentication related Counters', ['diameter_application_id', 'diameter_cmd_code', 'event', 'imsi_prefix'])
+prom_diam_eir_event_count = Counter('prom_diam_eir_event_count', 'Diameter EIR event related Counters', ['response'])
 
 prom_http_geored = Counter('prom_http_geored', 'Number of Geored Pushes', ['geored_host', 'endpoint', 'http_response_code', 'error'])
 prom_flask_http_geored_endpoints = Counter('prom_flask_http_geored_endpoints', 'Number of Geored Pushes Received', ['geored_host', 'endpoint'])
