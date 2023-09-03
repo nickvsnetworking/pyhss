f08dad09945e95ee32332ec41d3f4432c20a2004
nickvsnetworking <nick@nickvsnetworking.com>
2023-09-03 11:21:53 +1000

    Fix invalid syntax

--- PyHSS_API.py
+++ PyHSS_API.py
@@ -460,8 +460,8 @@ class PyHSS_SUBSCRIBER_Get(Resource):
                         CancellationType=1
                     )
                     logObj = logtool.LogTool()
-                    diameter_next_hop = str(json_data['serving_mme_peer'].split(';')[0]
-                    logObj.Async_SendRequest(diam_hex, diameter_next_hop))
+                    diameter_next_hop = str(json_data['serving_mme_peer']).split(';')[0]
+                    logObj.Async_SendRequest(diam_hex, diameter_next_hop)
                     print("Sent CLR via Peer " + str(diameter_next_hop))
                 else:
                     print("No serving MME set - Not sending CLR")
