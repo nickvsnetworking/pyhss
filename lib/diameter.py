7fe267892c96903231d509ebd7826b29a38b2c96
bigsheykh <36641078+bigsheykh@users.noreply.github.com>
2025-04-22 19:17:56 +0330

    add Destination Realm to CCR diameter.

--- lib/diameter.py
+++ lib/diameter.py
@@ -4783,6 +4783,9 @@ class Diameter:
         avp += self.generate_avp(277, 40, "00000001")                                                    #Auth-Session-State (Not maintained)        
         avp += self.generate_avp(264, 40, self.OriginHost)                                               #Origin Host
         avp += self.generate_avp(296, 40, self.OriginRealm)                                              #Origin Realm
+        avp += self.generate_avp(283, 40, self.string_to_hex(destinationRealm))                          #Destination Realm
+        # avp += self.generate_avp(293, 40, self.string_to_hex(destinationHost))                         #Destination Host
+
         
         avp += self.generate_avp(258, 40, format(int(16777238),"x").zfill(8))   #Auth-Application-ID Gx
 
