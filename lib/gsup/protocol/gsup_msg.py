51a3f5a464d2e614c48f5c297f060c04e35124d1
Oliver Smith <osmith@sysmocom.de>
2026-08-06 11:47:04 +0200

    lib/gsup/protocol/gsup_msg: format with ruff

--- lib/gsup/protocol/gsup_msg.py
+++ lib/gsup/protocol/gsup_msg.py
@@ -9,19 +9,19 @@ from osmocom.gsup.message import MsgType, GsupMessage
 class GsupMessageBuilder:
     def __init__(self):
         self.gsup_dict = dict()
-        self.gsup_dict['ies'] = list()
-        self.gsup_dict['msg_type'] = ""
+        self.gsup_dict["ies"] = list()
+        self.gsup_dict["msg_type"] = ""
 
     def with_msg_type(self, msg_type: MsgType):
-        self.gsup_dict['msg_type'] = msg_type.name
+        self.gsup_dict["msg_type"] = msg_type.name
         return self
 
     def with_ie(self, name: str, value, merge: bool = True):
-        if 'ies' not in self.gsup_dict:
-            self.gsup_dict['ies'] = []
+        if "ies" not in self.gsup_dict:
+            self.gsup_dict["ies"] = []
 
         if merge:
-            for ie in self.gsup_dict['ies']:
+            for ie in self.gsup_dict["ies"]:
                 if name in ie and isinstance(ie[name], list) and isinstance(value, dict):
                     ie[name].append(value)
                     return self
@@ -29,43 +29,36 @@ class GsupMessageBuilder:
                     ie[name].extend(value)
                     return self
 
-        self.gsup_dict['ies'].append({
-            name: value
-        })
+        self.gsup_dict["ies"].append({name: value})
         return self
 
     def with_msisdn_ie(self, msisdn: str):
-        ie = {
-            'bcd_len': (len(msisdn) + 1) // 2,
-            'digits': msisdn
-        }
-        return self.with_ie('msisdn', ie)
+        ie = {"bcd_len": (len(msisdn) + 1) // 2, "digits": msisdn}
+        return self.with_ie("msisdn", ie)
 
     def with_pdp_info_ie(self, pdp_ctx_id: int, pdp_type: str, apn_name: str):
         pdp_info = []
 
-        pdp_info.append({
-            'pdp_context_id': pdp_ctx_id
-        })
+        pdp_info.append({"pdp_context_id": pdp_ctx_id})
 
-        pdp_info.append({
-            'pdp_address': {
-                'address': None,
-                'hdr': {
-                    'pdp_type_nr': pdp_type,
-                    'pdp_type_org': 'ietf'
-                }
+        pdp_info.append(
+            {
+                "pdp_address": {
+                    "address": None,
+                    "hdr": {
+                        "pdp_type_nr": pdp_type,
+                        "pdp_type_org": "ietf",
+                    },
+                },
             }
-        })
+        )
 
-        pdp_info.append({
-            'access_point_name': apn_name
-        })
+        pdp_info.append({"access_point_name": apn_name})
 
-        return self.with_ie('pdp_info', pdp_info, False)
+        return self.with_ie("pdp_info", pdp_info, False)
 
     def build(self) -> GsupMessage:
-        if self.gsup_dict['msg_type'] == "":
+        if self.gsup_dict["msg_type"] == "":
             raise ValueError("msg_type is required")
         return GsupMessage.from_dict(self.gsup_dict)
 
@@ -77,7 +70,7 @@ class GsupMessageUtil:
 
     @staticmethod
     def get_first_ie_by_name(ie_name: str, message: dict):
-        for ie in message['ies']:
+        for ie in message["ies"]:
             if ie_name in ie:
                 return ie[ie_name]
         return None
@@ -85,7 +78,7 @@ class GsupMessageUtil:
     @staticmethod
     def get_ies_by_name(ie_name: str, message: dict):
         ies = []
-        for ie in message['ies']:
+        for ie in message["ies"]:
             if ie_name in ie:
                 ies.append(ie)
         return ies
@@ -107,12 +100,12 @@ class GMMCause(Enum):
     GPRS_NOTALLOWED = 0x07
     GPRS_OTHER_NOTALLOWED = 0x08
     MS_ID_NOT_DERIVED = 0x09
-    IMPL_DETACHED = 0x0a
-    PLMN_NOTALLOWED = 0x0b
-    LA_NOTALLOWED = 0x0c
-    ROAMING_NOTALLOWED = 0x0d
-    NO_GPRS_PLMN = 0x0e
-    NO_SUIT_CELL_IN_LA = 0x0f
+    IMPL_DETACHED = 0x0A
+    PLMN_NOTALLOWED = 0x0B
+    LA_NOTALLOWED = 0x0C
+    ROAMING_NOTALLOWED = 0x0D
+    NO_GPRS_PLMN = 0x0E
+    NO_SUIT_CELL_IN_LA = 0x0F
     MSC_TEMP_NOTREACH = 0x10
     NET_FAIL = 0x11
     MAC_FAIL = 0x14
@@ -120,13 +113,13 @@ class GMMCause(Enum):
     CONGESTION = 0x16
     GSM_AUTH_UNACCEPT = 0x17
     NOT_AUTH_FOR_CSG = 0x19
-    SMS_VIA_GPRS_IN_RA = 0x1c
+    SMS_VIA_GPRS_IN_RA = 0x1C
     NO_PDP_ACTIVATED = 0x28
-    SEM_INCORR_MSG = 0x5f
+    SEM_INCORR_MSG = 0x5F
     INV_MAND_INFO = 0x60
     MSGT_NOTEXIST_NOTIMPL = 0x61
     MSGT_INCOMP_P_STATE = 0x62
     IE_NOTEXIST_NOTIMPL = 0x63
     COND_IE_ERR = 0x64
     MSG_INCOMP_P_STATE = 0x65
-    PROTO_ERR_UNSPEC = 0x6f
+    PROTO_ERR_UNSPEC = 0x6F
