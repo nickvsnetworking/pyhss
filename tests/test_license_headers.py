40db7201a69d7c5ca516327bb580f7e8aee3675a
Oliver Smith <osmith@sysmocom.de>
2026-08-10 10:57:47 +0200

    tests/test_license_headers: remove leftover code

--- tests/test_license_headers.py
+++ tests/test_license_headers.py
@@ -33,10 +33,6 @@ def test_license_headers():
     for ext in extensions:
         pattern = os.path.join(top_dir, f"**/*.{ext}")
         for i in glob.glob(pattern, recursive=True):
-            if os.path.relpath(i, top_dir).startswith("tools/databaseUpgrade"):
-                # Will be removed in this PR, not worth adjusting:
-                # https://github.com/nickvsnetworking/pyhss/pull/297
-                continue
             if not file_has_headers(i):
                 missing += [i]
 
