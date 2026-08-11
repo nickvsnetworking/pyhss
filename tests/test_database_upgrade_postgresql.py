64d3400028b928c0e67ee96eba94b75def901a37
Oliver Smith <osmith@sysmocom.de>
2026-08-11 13:06:59 +0200

    tests: postgres: try createdb multiple times
    I've seen the createdb command fail occasionally in github's CI.
    Apparently postgres can be in a "starting up" state even after the tcp
    port is bound:
    
      createdb: error: connection to server at "127.0.0.1", port 54321 failed: FATAL:  the database system is starting up
    
    Make the tests pass reliably by running the createdb command up to 5
    times with a 1s sleep between each attempt.
    
    Related: https://github.com/nickvsnetworking/pyhss/actions/runs/31482527502/job/93750506691

--- tests/test_database_upgrade_postgresql.py
+++ tests/test_database_upgrade_postgresql.py
@@ -4,6 +4,7 @@ import glob
 import importlib
 import os
 import subprocess
+import time
 from pathlib import Path
 
 import pytest
@@ -41,9 +42,20 @@ def run_postgresql(tmpdir):
 
     try:
         wait_for_tcp_port(postgresql_port)
-        cmd = [createdb[0], "-h127.0.0.1", f"-p{postgresql_port}", "-Uuser", "hss"]
-        print(f"+ {cmd}")
-        subprocess.run(cmd, check=True)
+        tries = 5
+        for i in range(tries):
+            cmd = [createdb[0], "-h127.0.0.1", f"-p{postgresql_port}", "-Uuser", "hss"]
+            print(f"+ {cmd}")
+            createdb_proc = subprocess.run(cmd, check=False)
+            if createdb_proc.returncode == 0:
+                break
+            print(f"createdb failed ({i + 1}/{tries}) with returncode: {createdb_proc.returncode}")
+            if i + 1 < tries:
+                print("Database might still be starting up, trying again in 1s…")
+                time.sleep(1)
+            else:
+                print("Giving up")
+                raise RuntimeError("createdb failed!")
         yield
     finally:
         proc.kill()
